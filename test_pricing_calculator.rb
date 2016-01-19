e1d524a2b8c2e0ce2fd0692f560d4a11e6a3e92b
margaretyjkim <margaret.yj.kim@gmail.com>
2016-01-19 11:59:22 -0500

    added second input test

--- test_pricing_calculator.rb
+++ test_pricing_calculator.rb
@@ -7,4 +7,9 @@ class TestPricing < MiniTest::Test
 		assert_equal 1591.58, calculate_markup(1299.99, 3, "food")
 	end
 
+	def test_second_input
+		assert_equal 6199.81, calculate_markup(5432.00, 1, "drugs")
+	end
+
+
 end
