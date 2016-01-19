0d35ab3efb0c8da5134907c067e9b80eb5da00f5
margaretyjkim <margaret.yj.kim@gmail.com>
2016-01-19 11:56:41 -0500

    added first input test

--- test_pricing_calculator.rb
+++ test_pricing_calculator.rb
@@ -3,4 +3,8 @@ require './test_helper'
 require 'pricing_calculator'
 
 class TestPricing < MiniTest::Test
+	def test_first_input
+		assert_equal 1591.58, calculate_markup(1299.99, 3, "food")
+	end
+
 end
